4f7c9c07b57e5038ddc807c3b441babf747e6c02
ismael123ismael3-cmd <jhery1005@gmail.com>
2026-09-20 12:00:43 -0400

    asdasd

--- Trabajos/trabajo Video final/Ejercicio.cs
+++ Trabajos/trabajo Video final/Ejercicio.cs
@@ -8,7 +8,36 @@ namespace Trabajos.trabajo_Video_final
     {
         public static void Main(string[] args)
         {
+            //Console.WriteLine("Bienvenido a Programacion II - Clase02");
+            //saludar();
+            funcion1.saludar();
+            //Introducir Datos
+            funcion1.datos();
+
+            Console.WriteLine("Por favor introduce un valor para comprobar la calculadora en a");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Por favor introduce un valor para comprobar la calculadora en b");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("La suma es" + " " + funcion1.Sumar(a, b));
+            Console.WriteLine("La suma es" + " " + funcion1.Restar(a, b));
+            Console.WriteLine("La suma es" + " " + funcion1.Multiplicar(a, b));
+            Console.WriteLine("La suma es" + " " + funcion1.Dividir(a, b));
+
+
+            /*
+            Console.WriteLine("La suma es" +" " + Sumar(a, b));
+            Console.WriteLine("La resta es" + " " + Restar(a, b));
+            Console.WriteLine("La multiplicacion es" + " " + Multiplicar(a, b));
+            Console.WriteLine("La division es" + " " + Dividir(a, b));
+            */
+                    /*Metodos
+                    * 
+                    * 
+                    *
+
 
         }
+    }
     }
 }
---
+++ Trabajos/trabajo Video final/funcion1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trabajos.trabajo_Video_final
+{
+    public class funcion1
+    {
+        public static void saludar()
+        {
+            Console.WriteLine("Bienvenido a la clase nro 2");
+        }
+
+
+
+        public static void datos()
+        {
+            Console.WriteLine("Por favor dime tu nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Bienvenido " + nombre);
+            Console.Write("Dime por favor cuantos años de experiencia tienes en c#");
+            int experiencia = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dime un aproximado de tiempo que le dedicas a la programacion");
+            double tiempo = double.Parse(Console.ReadLine());
+            Console.WriteLine("Muchas gracias " + nombre + " " + "Tu experiencia de " + experiencia + "años" + " " + " el tiempo que le dedicas  " + " " + tiempo + "min " + " " +
+                                " sera un pilar fundamental para el desarrollo de la clase");
+        }
+        */
+
+
+        /*
+         * Funciones
+         */
+        public static double Sumar(double a, double b)
+        {
+            return a + b;
+
+        }
+
+        public static double Restar(double a, double b)
+        {
+            return a - b;
+
+        }
+
+        public static double Multiplicar(double a, double b)
+        {
+            return a * b;
+
+        }
+
+        public static double Dividir(double a, double b)
+        {
+            return a / b;
+
+        }
+
+    }
+}
