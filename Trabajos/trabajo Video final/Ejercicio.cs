80a3bc06a2c0ddb06d72bd7125e9792362317379
ismael123ismael3-cmd <jhery1005@gmail.com>
2026-09-20 11:48:40 -0400

    asdas

---
+++ Trabajos/trabajo Video final/Ejercicio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trabajos.trabajo_Video_final
+{
+    public class Ejercicio
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
