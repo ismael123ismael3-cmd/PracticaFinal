454c3cb7e164e7cece7d074f3b87e679c1147824
ismael123ismael3-cmd <jhery1005@gmail.com>
2026-09-20 22:22:18 -0400

    asdas

---
+++ Trabajos/Grabacion/Funcion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trabajos.Grabacion
+{
+    public class Funcion
+    {
+        public static int kilosDisponibles;
+
+        public static void CalcularRacionesComida()
+        {
+            if (kilosDisponibles < 50)
+            {
+                Console.WriteLine("RACION DE EMERGENCIA:Solo se autoriza 1/4 de racion por soldado");
+                Console.WriteLine("notificacion: Reservas en estado critico");
+            }
+            else if (kilosDisponibles <= 150)
+            {
+                Console.WriteLine("RACION REDUCIDA:Se autoriza media racion por soldado");
+                Console.WriteLine("notificacion:Controlar el consumo diario consumo diario");
+            }
+            else
+            {
+                Console.WriteLine("RACION COMPLETA:Se autoriza racion normal para la tropa");
+                Console.WriteLine("Norificacion:Suministros en buen estado");
+            }
+        }
+    }
+
+}
---
+++ Trabajos/Grabacion/Principal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trabajos.Grabacion
+{
+    internal class Principal
+    {
+        public static void Main(string[] args)
+        {
+            int OpcionContinuar;
+
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("SISTEMA DE RACIONAMIENTO - FORTIN NANAWA (Chaco 1933)");
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++");
+
+            do
+            {
+                Console.WriteLine("--- REGISTRO DIARIO DE RECURSOS ---");
+                Console.WriteLine("Ingresar la cantidad de comida disponible (en kilos)");
+
+                Funcion.kilosDisponibles = int.Parse(Console.ReadLine());
+
+                Funcion.CalcularRacionesComida();
+
+                do
+                {
+                    Console.WriteLine("Desea Registrar el racionamiento del siguiente dia (1:Si/0:No)");
+                    OpcionContinuar = int.Parse(Console.ReadLine());
+
+                    if (OpcionContinuar != 0 && OpcionContinuar != 1) ;
+                    {
+                        Console.WriteLine("Opcion Invalida. Elija de nuevo");
+                    }
+                }
+                while (OpcionContinuar != 0 && OpcionContinuar != 1);
+            }
+            while (OpcionContinuar != 0);
+        }
+    }
+}
